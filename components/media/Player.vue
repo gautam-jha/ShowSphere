600c7a50d3da38a297d43402849a461d24dc4748
Gautam Jha <gatamjha.mailbox@gmail.com>
2023-09-22 23:58:54 +0530

    Minor fix

--- components/media/Player.vue
+++ components/media/Player.vue
@@ -99,7 +99,7 @@ const seasons = props.item?.seasons
         </div>
       </div>
     </CarouselBase>
-    <div flex justify-center>
+    <div v-if="type === 'tv' && data.selectedSeason" flex justify-center>
       <fieldset class="grid grid-cols-2 gap-4 w-80">
         <div v-for="i in [1, 2]" :key="i">
           <input
@@ -135,38 +135,6 @@ const seasons = props.item?.seasons
     
           </label>
         </div>
-        <!--     
-        <div>
-          <input
-            id="Server2"
-            type="radio"
-            name="ser2"
-            value="Server2"
-            class="peer hidden [&:checked_+_label_svg]:block"
-          >
-    
-          <label
-            for="Server2"
-            class="block cursor-pointer rounded-lg border border-gray-100 bg-white p-4 text-sm font-medium shadow-sm hover:border-gray-200 peer-checked:border-blue-500 peer-checked:ring-1 peer-checked:ring-blue-500"
-          >
-            <div class="flex items-center justify-between">
-              <p class="text-gray-700">Server 2</p>
-    
-              <svg
-                class="hidden h-5 w-5 text-blue-600"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </div>
-          </label>
-        </div> -->
       </fieldset>
     </div>
     <div
